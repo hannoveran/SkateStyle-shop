a1bd647e9628e95263955de9c05378e3a6b43135
hannoveran <mamcukanna@gmail.com>
2025-09-26 15:00:44 +0300

    Fix links in navbar

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,6 +1,6 @@
 function createNavbar(options = {}) {
     const defaults = {
-        logoSrc: '/assets/images/logo/logoRemoveBg.png',
+        logoSrc: './assets/images/logo/logoRemoveBg.png',
         logoAlt: 'Skate Style Store Logo',
         cartCount: 0,
         currentPage: '' 
@@ -11,7 +11,7 @@ function createNavbar(options = {}) {
     return `
         <section class="navbar">
             <div class="logo">
-                <a href="/index.html">
+                <a href="./index.html">
                     <img src="${config.logoSrc}" alt="${config.logoAlt}">
                 </a>
             </div>
@@ -19,11 +19,11 @@ function createNavbar(options = {}) {
             <div class="nav-links main-links">
                 <nav>
                     <ul>
-                        <li><a href="/pages/category.html#type=women" ${config.currentPage === 'women' ? 'class="active"' : ''}>Жінкам</a></li>
-                        <li><a href="/pages/category.html#type=men" ${config.currentPage === 'men' ? 'class="active"' : ''}>Чоловікам</a></li>
-                        <li><a href="/pages/category.html#type=skates" ${config.currentPage === 'skates' ? 'class="active"' : ''}>Ковзани</a></li>
-                        <li><a href="/pages/category.html#type=accessories" ${config.currentPage === 'accessories' ? 'class="active"' : ''}>Аксесуари</a></li>
-                        <li><a href="/pages/aboutUs.html" ${config.currentPage === 'about' ? 'class="active"' : ''}>Про нас</a></li>
+                        <li><a href="./pages/category.html#type=women" ${config.currentPage === 'women' ? 'class="active"' : ''}>Жінкам</a></li>
+                        <li><a href="./pages/category.html#type=men" ${config.currentPage === 'men' ? 'class="active"' : ''}>Чоловікам</a></li>
+                        <li><a href="./pages/category.html#type=skates" ${config.currentPage === 'skates' ? 'class="active"' : ''}>Ковзани</a></li>
+                        <li><a href="./pages/category.html#type=accessories" ${config.currentPage === 'accessories' ? 'class="active"' : ''}>Аксесуари</a></li>
+                        <li><a href="./pages/aboutUs.html" ${config.currentPage === 'about' ? 'class="active"' : ''}>Про нас</a></li>
                         <li><a href="#phone-number">Контакти</a></li>
                     </ul>
                 </nav>
@@ -34,18 +34,18 @@ function createNavbar(options = {}) {
                     <ul>
                         <li class="search">
                             <a href="#" onclick="toggleSearch(event)">
-                                <img src="/assets/images/index/search.png" alt="Search">
+                                <img src="./assets/images/index/search.png" alt="Search">
                                 <span>Шукати</span>
                             </a>
                         </li>
                         <li>
-                            <a href="/pages/user/account.html">
-                                <img src="/assets/images/index/account.png" alt="Account">
+                            <a href="./pages/user/account.html">
+                                <img src="./assets/images/index/account.png" alt="Account">
                             </a>
                         </li>
                         <li class="cart">
                             <a href="#" onclick="toggleCart(event)">
-                                <img src="/assets/images/index/cart.png" alt="Cart">
+                                <img src="./assets/images/index/cart.png" alt="Cart">
                             </a>
                             <span id="cart-count">${config.cartCount}</span>
                         </li>
